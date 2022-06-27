f87f7676b632d936e811463b6648312a2058d086
JoAnisky <anisky5@gmail.com>
2022-06-27 11:44:24 +0200

    Ajout de l'insertion des données dans la base de données

--- add.php
+++ add.php
@@ -1,20 +1,54 @@
 <?php
+
+// Charger le fichier autoload.php (pour gérer le fichier .env)
+require __DIR__ .'/vendor/autoload.php';
+// Spécifie d'aller chercher le fichier .env (_DIR_, nomfichier(si il possède un nom))
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+
+// Charger la méthode dotenv
+$dotenv->load();
+
+// Récupère les variables contenue dans le fichier .env
+$DB_HOST= $_ENV['DB_HOST'];
+$DB_USER= $_ENV['DB_USER'];
+$DB_PASS= $_ENV['DB_PASS'];
+$DB_NAME= $_ENV['DB_NAME'];
+
 // Si le formulaire a bien été soumis
 if (isset($_POST["email"])) {
-    $email = $_POST["email"]
     // Si le champ mail n'est pas vide
-    if (!empty($email)) {
-        if(filter_var($email, FILTER_VALIDATE_EMAIL)){
+    if (!empty($_POST["email"])) {
+        if(filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
             echo "L'adresse e-mail est valide";
-            $pdo = new PDO('mysql:dbname=;host=127.0.0.1', '', '', [
-                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-            ]);
 
+            try{
+                // Initialise un objet PDO avec les données de connexions transmises depuis le fichier .env
+                $dbco = new PDO('mysql:dbname='.$DB_NAME.';host='.$DB_HOST, $DB_USER, $DB_PASS);
+
+                // Définit les paramètres d'exceptions
+                $dbco->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+                // Ajout une sécurité (on précise le type de données attendu)
+                $dbco->bindParam(':mail', $_POST["email"], PDO::PARAM_STR);
+
+                // Prépare la requête SQL
+                $insertDB = $dbco->prepare("
+                    INSERT INTO subscribes (mail) 
+                    VALUES (:mail)
+                    ");
+                $insertDB->execute([
+                    'mail' => $_POST["email"]
+                ]);
+            
+            } catch (PDOExeption $e){
+                echo $e->getMessage();
+            }
         }else{
             echo "L'adresse e-mail n'est pas valide";
         }
     }
 }
+
 echo "Formulaire non envoyé !"
 
 ?>
