8cf16ba753e6df1417cb450eea2f33dc99b93f54
JoAnisky <anisky5@gmail.com>
2022-06-27 16:39:06 +0200

    Formulaire de traitement page login en JS

--- js/login.js
+++ js/login.js
@@ -1,35 +1,35 @@
-console.log('coucou');
 const form = document.querySelector("form");
 const inputLogin = document.getElementById("login");
 const inputPassword = document.getElementById("password");
 const labelErrorLogin = document.getElementById('error-login');
 const labelErrorPassword = document.getElementById('error-password');
-console.log(inputPassword);
+console.log(labelErrorPassword);
 
 form.addEventListener("submit", function(e){
     e.preventDefault();
     if (inputLogin.validity.valueMissing){
-        label.textContent = "Ce champ ne peut pas être vide !";
-        label.style.color = "red";
+        labelErrorLogin.textContent = "Login ne peut pas être vide !";
+        labelErrorLogin.style.color = "red";
         inputLogin.style.border = "1px solid red";
     }else if (inputPassword.validity.valueMissing){
-        label.textContent = "Ce champ ne peut pas être vide !";
-        label.style.color = "red";
+        labelErrorPassword.textContent = "Password ne peut pas être vide !";
+        labelErrorPassword.style.color = "red";
         inputLogin.style.border = "1px solid red";
     }else{
         ajaxResponse();
-        label.textContent = "";
+        labelErrorLogin.textContent = "";
+        labelErrorPassword.textContent = "";
         label.style.color = '#15ff00';
     }
 });
 
-// function ajaxResponse(){
-//     const formData = new FormData(form);
-//     // Lancement de la requête AJAX si tout est OK coté JS
-//     fetch('admin.php', {
-//         method: "POST",
-//         body : formData
-//     }).then(response => response.text())
-//     .then(response=> {
-//     });
-// };
+function ajaxResponse(){
+    const formData = new FormData(form);
+    // Lancement de la requête AJAX si tout est OK coté JS
+    fetch('admin.php', {
+        method: "POST",
+        body : formData
+    }).then(response => response.text())
+    .then(response=> {
+    });
+};
