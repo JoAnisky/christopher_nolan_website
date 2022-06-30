b8525d5cf52088b7e763c9c4ae1210826f5c8002
JoAnisky <anisky5@gmail.com>
2022-06-30 15:40:24 +0200

    Creation du script JS et corrections de bug d'affichage bouton deconnection

---
+++ js/dashboard.js
@@ -0,0 +1,8 @@
+// const disconnectBtn = document.getElementById("logo-disconnect");
+
+// disconnectBtn.addEventListener('click', disconnect);
+
+// function disconnect(){
+//     var result="<?php php_func(); ?>"
+//     console.log(result);
+// }
